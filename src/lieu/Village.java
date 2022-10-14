f4a54078a6ee562eb521b6e1b08070e89bdef264
popoulpe <54855619+popo980@users.noreply.github.com>
2022-10-14 09:45:27 +0200

    TP1  test

--- src/lieu/Village.java
+++ src/lieu/Village.java
@@ -4,11 +4,17 @@ import personnage.Chef;
 public class Village {
 	public String nom;
 	public Chef chef;
+	public int nbVillageois;
+	public int nbVillageoisMax;
+	public String[] villageois;
 	
 	
-	public Village(String nom) {
+	public Village(String nom, Chef chef, int nbVillageoisMax) {
 		this.nom = nom;
 		this.chef = chef;
+		this.nbVillageois = 0;
+		this.nbVillageoisMax = nbVillageoisMax;
+		this.villageois = new String[nbVillageoisMax];
 	}
 	
 	public void setChef(Chef chef){
