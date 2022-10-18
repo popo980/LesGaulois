79afdfdd42d1b33f96201a13cb9bee81eadd9cab
popoulpe <paulinlaurent@orange.fr>
2022-10-18 09:34:26 +0200

    invariant pre post conditions

--- src/personnage/Romain.java
+++ src/personnage/Romain.java
@@ -4,9 +4,13 @@ package personnage;
 public class Romain  { 
 	private String nom; 
 	private int force; 
+	
 	public Romain(String nom, int force) { 
+		
 		this.nom = nom; 
 		this.force = force; 
+		
+		assert force > 0 : "force inferieur negative";
 	} 
 	
 	public String getNom() { 
@@ -21,20 +25,23 @@ public class Romain  {
 		return "Le romain " + nom + " : "; 
 	} 
 	
-	public void recevoirCoup(int forceCoup) { 
+	public void recevoirCoup(int forceCoup) {
+		assert force > 0 : "force inferieur negative";
+		int forceDebut = force;
 		force -= forceCoup; 
 		if (force > 0) { 
 			this.parler("Aie"); 
 		} else { 
 			this.parler("J'abandonne..."); 
 		} 
+		assert forceDebut > force : "force la force n'a pas diminué en prenant un coup";
 	} 
 	
 	public static void main(String[] args) {
-		Romain philipus = new Romain("philipus", 3);
-		philipus.prendreParole();
-		philipus.parler("test");
-		philipus.recevoirCoup(5);
+		Romain minus = new Romain("minus", 6);
+		minus.prendreParole();
+		minus.parler("test");
+		minus.recevoirCoup(5);
 		
 	}
 } 
